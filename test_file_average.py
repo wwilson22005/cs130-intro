85e626a118890d5a4b345e6e5fc67ca2632d392f
Deva <41238619+devaoneil@users.noreply.github.com>
2024-07-21 11:32:54 -0400

    Update test_file_average.py

--- test_file_average.py
+++ test_file_average.py
@@ -1,10 +1,10 @@
-from averaging import average
+from averaging import average_2, average_3
 
-def avg_2():
-	assert average(1,2) == 1.5
-	assert average(-1, 1) == 0
+def test_avg_2():
+	assert average_2(1,2) == 1.5
+	assert average_2(-1, 1) == 0
 
-def avg_3():
-	assert average(0,1,2) == 1
-	assert average(-1, 0, 1) == 0
-	assert average(10,20,30) == 20
+def test_avg_3():
+	assert average_3(0,1,2) == 1
+	assert average_3(-1, 0, 1) == 0
+	assert average_3(10,20,30) == 20
